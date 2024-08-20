45070a12edb9f49e77b2afab5d75fc199dfc37fb
vincent-j-chang <vincentchang.dev@gmail.com>
2024-08-20 17:56:19 -0400

    update variables to check if the name is too long

--- variables.tf
+++ variables.tf
@@ -69,13 +69,24 @@ variable "private_subnet_cidr_blocks" {
 }
 
 variable "resource_tags" {
-    description = "Tags to set for all resources"
-    type = map(string)
-    default = {
-        project = "project-alpha"
-        environment = "dev"
-    }
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default     = {
+    project     = "my-project",
+    environment = "dev"
+  }
+
+  validation {
+    condition     = length(var.resource_tags["project"]) <= 16 && length(regexall("[^a-zA-Z0-9-]", var.resource_tags["project"])) == 0
+    error_message = "The project tag must be no more than 16 characters, and only contain letters, numbers, and hyphens."
+  }
+
+  validation {
+    condition     = length(var.resource_tags["environment"]) <= 8 && length(regexall("[^a-zA-Z0-9-]", var.resource_tags["environment"])) == 0
+    error_message = "The environment tag must be no more than 8 characters, and only contain letters, numbers, and hyphens."
+  }
 }
+
 
 variable "ec2_instance_type" {
     description = "AWS EC2 instance type"
